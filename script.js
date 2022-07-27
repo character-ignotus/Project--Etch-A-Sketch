2d2d8f327aa154af11911c20d11e948b64aab44d
Lyubomir Lyubomirov <l.lubomirov00@gmail.com>
2022-07-27 16:18:40 +0300

    Add a grid generation button

--- script.js
+++ script.js
@@ -1,11 +1,36 @@
 const body = document.querySelector('body');
+
 const mainDiv = document.createElement('div');
 mainDiv.setAttribute('id', 'main-container');
-
 body.appendChild(mainDiv);
+
+const leftContainer = document.createElement('div');
+const rightContainer = document.createElement('div');
+
+leftContainer.classList.add('container');
+rightContainer.classList.add('container');
+
+body.insertBefore(leftContainer, mainDiv);
+body.appendChild(rightContainer);
+
+const gridGenerationBtn = document.createElement('button');
+gridGenerationBtn.textContent = 'Generate Grid';
+
+rightContainer.appendChild(gridGenerationBtn);
 
 let gridNumber = 0;
 const subDivsArray = [];
+
+
+gridGenerationBtn.addEventListener('click', () => {
+    while(subDivsArray.length > 0) {
+        subDivsArray.pop();
+    }
+
+    mainDiv.replaceChildren();
+
+    gridGeneration();
+});
 
 function gridGeneration() {
     gridNumber = +prompt('Enter grid number');
@@ -24,13 +49,12 @@ function gridGeneration() {
     subDivsArray.forEach((item) => {
         mainDiv.appendChild(item);
     });
+
+    let squares = Array.from(document.getElementsByClassName('square'));
+        squares.forEach((block) => {
+        block.addEventListener('mouseenter', () => {
+            block.style.backgroundColor = 'black';
+        });
+    });
 }
 
-gridGeneration();
-
-let squares = Array.from(document.getElementsByClassName('square'));
-squares.forEach((block) => {
-    block.addEventListener('mouseenter', () => {
-        block.style.backgroundColor = 'black';
-    });
-});
