44bb9163871af5fd2394e03e4b95bcd4693bb39f
Lyubomir Lyubomirov <l.lubomirov00@gmail.com>
2022-07-27 15:47:36 +0300

    Add a function that generates an NxN grid

---
+++ script.js
@@ -0,0 +1,29 @@
+const body = document.querySelector('body');
+const mainDiv = document.createElement('div');
+mainDiv.setAttribute('id', 'main-container');
+
+body.appendChild(mainDiv);
+
+let gridNumber = 0;
+const subDivsArray = [];
+
+function gridGeneration() {
+    gridNumber = +prompt('Enter grid number');
+
+    for(i=0; i<gridNumber; i++) {
+        let subDiv = document.createElement('div');
+        subDiv.classList.add('subContainer')
+        for(j=0; j<gridNumber; j++) {
+            let square = document.createElement('div');
+            square.classList.add('square');
+            subDiv.appendChild(square);
+        }
+        subDivsArray[i] = subDiv;
+    }
+
+    subDivsArray.forEach((item) => {
+        mainDiv.appendChild(item);
+    });
+}
+
+gridGeneration()
